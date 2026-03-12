47be2dcad01f4b4e598da291aea8e4c0ce527557
bouteldja Abdelfatah <300156204@monboreal.ca>
2026-03-12 18:40:41 -0400

    Implémentation de la méthode sortirVoiture

--- Atelier3.py
+++ Atelier3.py
@@ -3,19 +3,16 @@ class Voiture:
         self.matricule = matricule
         self.marque = marque
         self.couleur = couleur
-
     def afficher_infos(self):
         print("Matricule :", self.matricule)
         print("Marque :", self.marque)
         print("Couleur :", self.couleur)
-
 class Parc:
     def _init_(self,id,adresse,capacite marque,iste_voitures):
         self.id = id
         self.adresse = adresse
         self.capacite = capacite
         self.liste_voitures = liste_voitures):
-
     def afficher_infos(self):
         print("ID :", self.id)
         print("Adresse :", self.adresse)
@@ -24,7 +21,6 @@ class Parc:
         for v in self.liste_voitures:
             v.afficher_infos()
             print("------")
-
     def entrerVoiture(self, voiture):
         if voiture in self.liste_voitures:
             print("La voiture existe deja dans le parc")
@@ -33,4 +29,10 @@ class Parc:
         else:
             self.liste_voitures.append(voiture)
             print("Voiture ajoutee au parc")
+    def sortirVoiture(self, voiture):
+        if voiture not in self.liste_voitures:
+            print("La voiture n'est pas dans le parc")
+        else:
+            self.liste_voitures.remove(voiture)
+            print("Voiture retiree du parc")
 
