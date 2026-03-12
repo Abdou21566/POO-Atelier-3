2bbf1583e5f6c85bd1bf3788b145b58f0a8bae70
bouteldja Abdelfatah <300156204@monboreal.ca>
2026-03-12 18:14:08 -0400

    Implémenter les méthodes de la classe Voiture

--- Atelier3.py
+++ Atelier3.py
@@ -3,3 +3,13 @@ class Voiture:
         self.matricule = matricule
         self.marque = marque
         self.couleur = couleur
+class Voiture:
+    def _init_(self, matricule, marque, couleur):
+        self.matricule = matricule
+        self.marque = marque
+        self.couleur = couleur
+
+    def afficher_infos(self):
+        print("Matricule :", self.matricule)
+        print("Marque :", self.marque)
+        print("Couleur :", self.couleur)
