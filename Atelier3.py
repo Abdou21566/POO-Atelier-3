43a5ee869a4067a4b55e04cfe987d3db7a7ff235
bouteldja Abdelfatah <300156204@monboreal.ca>
2026-03-12 18:19:52 -0400

    Créer la classe Parc et ses attributs

--- Atelier3.py
+++ Atelier3.py
@@ -3,13 +3,15 @@ class Voiture:
         self.matricule = matricule
         self.marque = marque
         self.couleur = couleur
-class Voiture:
-    def _init_(self, matricule, marque, couleur):
-        self.matricule = matricule
-        self.marque = marque
-        self.couleur = couleur
 
     def afficher_infos(self):
         print("Matricule :", self.matricule)
         print("Marque :", self.marque)
         print("Couleur :", self.couleur)
+
+class Parc:
+    def _init_(self,id,adresse,capacite marque,iste_voitures):
+        self.id = id
+        self.adresse = adresse
+        self.capacite = capacite
+        self.liste_voitures = liste_voitures):
