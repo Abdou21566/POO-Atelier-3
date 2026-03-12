dce21bf064abaed9180b0a4b704bbb3ee1d2ecab
bouteldja Abdelfatah <300156204@monboreal.ca>
2026-03-12 18:24:38 -0400

    Implémenter la méthode entrerVoiture

--- Atelier3.py
+++ Atelier3.py
@@ -15,3 +15,12 @@ class Parc:
         self.adresse = adresse
         self.capacite = capacite
         self.liste_voitures = liste_voitures):
+
+    def entrerVoiture(self, voiture):
+        if voiture in self.liste_voitures:
+            print("La voiture existe deja dans le parc")
+        elif len(self.liste_voitures) >= self.capacite:
+            print("Le parc est plein")
+        else:
+            self.liste_voitures.append(voiture)
+            print("Voiture ajoutee au parc")
